2732b3425aade578ea327a86fd50ee55a4f9acdb
snaidy giraldo <snaidy98g@gmail.com>
2023-07-26 15:55:50 -0600

    add structure asientos pago descarga transaccion and changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,16 +5,24 @@ import Login from "../login/Login.jsx";
 import DetallePelicula from "../detallePelicula/DetallePelicula.jsx";
 import SeleccionAsientos from "../seleccionAsientos/SeleccionAsientos.jsx";
 import SeleccionBoletos from "../seleccionBoletos/SeleccionBoletos.jsx";
+import Carrusel from "../carrusel/Carrusel.jsx";
+import PagoBoletos from "../pagoBoletos/PagoBoletos.jsx";
+import TransaccionExitosa from "../transaccionExitosa/TransaccionExitosa.jsx";
+import DescargaBoletos from "../descargaBoletos/DescargaBoletos.jsx";
 const Home = () => {
 
 
   return (
     <div>
-      <Header />
+      <Header/>
+      <Carrusel/>
       {/* <Login/> */}
       {/* <DetallePelicula/> */}
       {/* <SeleccionBoletos/> */}
-      <SeleccionAsientos/>
+      {/* <SeleccionAsientos/> */}
+      {/* <PagoBoletos/> */}
+      {/* <TransaccionExitosa/> */}
+      <DescargaBoletos/>
     </div>
   );
 };
